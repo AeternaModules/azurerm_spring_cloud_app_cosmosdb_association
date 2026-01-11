ae547e2b07ca5bdd393f3532080b62c6fe5e8860
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:23:13 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "spring_cloud_app_cosmosdb_associations" {
+  description = "All spring_cloud_app_cosmosdb_association resources"
+  value       = azurerm_spring_cloud_app_cosmosdb_association.spring_cloud_app_cosmosdb_associations
+}
+
